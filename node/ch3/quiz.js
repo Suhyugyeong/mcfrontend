c64a26faaaf98666e7c5f3b160ab2ca9683a8d7e
Suhyugyeong <suhyugyeongsuh@gamil.com>
2023-12-15 09:10:52 +0900

    1215

--- node/ch3/quiz.js
+++ node/ch3/quiz.js
@@ -1,53 +1,54 @@
-//시스템 변수 설정 확인: YOUR_AES_KEY와 YOUR_AES_IV를 올바르게 시스템 변수로 설정했는지 확인합니다. 프로그램이 실행되는 환경에서 이 변수들이 정의되어 있어야 합니다.
-
 const fs = require("fs");
 const crypto = require("crypto");
 
-const algorithm = "aes-256-cbc";
+const readStream = fs.createReadStream("./quiz.txt", { highWaterMark: 16 });
+const data = [];
+readStream.on("data", (chunk) => {
+  data.push(chunk);
+  console.log("data:", chunk, chunk.length);
+});
+readStream.on("end", () => {
+  console.log("end:", Buffer.concat(data).toString());
+});
+readStream.on("error", (err) => {
+  console.log("error:", err);
+});
 
-// 시스템 변수에서 키와 IV 가져오기
+const algorithm = "aes-256-cbc";
 const key = process.env.YOUR_AES_KEY;
 const iv = process.env.YOUR_AES_IV;
-
-// 키와 IV가 없을 경우 오류 처리
 if (!key || !iv) {
   console.error("Error: Key or IV not provided.");
-  process.exit(1); // 프로그램 종료
+  process.exit(1);
 }
 
-// 키와 IV를 Buffer로 변환
-const keyBuffer = Buffer.from(key, "hex");
-const ivBuffer = Buffer.from(iv, "hex");
-
-// 암호화 함수
-function encrypt(text) {
-  const cipher = crypto.createCipheriv(algorithm, keyBuffer, ivBuffer);
-  let encrypted = cipher.update(text, "utf-8", "hex");
-  encrypted += cipher.final("hex");
-  return encrypted;
-}
-
-// 파일 읽기 스트림 및 쓰기 스트림
-const readStream = fs.createReadStream("./quiz.txt", { highWaterMark: 16 });
-const writeStream = fs.createWriteStream("quiz-chipher.txt");
-
-// 데이터 읽기 및 암호화 후 파일에 쓰기
-readStream.on("data", (chunk) => {
-  const encryptedChunk = encrypt(chunk);
-  writeStream.write(encryptedChunk, "utf-8");
+const cipher = crypto.createCipheriv(algorithm, key, iv);
+fs.readFile("./quiz.txt", "utf-8", (err, data) => {
+  if (err) {
+    console.error("Error reading file:", err);
+    return;
+  }
+  let result = cipher.update(data, "utf-8", "base64");
+  result += cipher.final("base64");
+  console.log("암호화 결과:", result);
 });
 
-readStream.on("end", () => {
-  writeStream.end();
-  console.log(
-    "Encryption complete. Encrypted text has been written to quiz-chipher.txt"
-  );
-});
+// 시스템 변수는 프로그램이 실행되는 환경에 미리 설정되어 있어야 합니다.
+// 터미널에서 아래와 같이 시스템 변수를 설정하고 코드를 실행할 수 있습니다.
+// export YOUR_AES_KEY=aaaaaaaaaaaaaaaaaaaaaaaaaaaaaaaa
+// export YOUR_AES_IV=aaaaaaaaaaaaaaaa
+// quiz.js
+// 위의 코드에서 your_script.js는 실제로 작성한 코드 파일의 이름으로 대체되어야 합니다.
+//이렇게 시스템 변수를 설정한 후 코드를 실행하면, 코드 내에서 해당 시스템 변수를 읽어와서 사용합니다.
 
-readStream.on("error", (err) => {
-  console.error("Error reading stream:", err);
+const writeStream = fs.createWriteStream("./quiz2.cipher.txt", "utf-8");
+writeStream.write(result);
+writeStream.end();
+
+writeStream.on("finish", () => {
+  console.log("암호화된 데이터를 파일에 쓰기 완료");
 });
 
 writeStream.on("error", (err) => {
-  console.error("Error writing to file:", err);
+  console.error("파일 쓰기 오류:", err);
 });
